fcb6410a13b9f907b73978d59e04383d96275a44
Heartsonsunday <leahgdg@hotmail.com>
2019-04-28 23:58:38 -0400

    updated api

--- api.js
+++ api.js
@@ -2,7 +2,29 @@
 const express = require('express');
 const router = express.Router();
 
-//get the input from the interface
-router.get('/input', function(req, res){
+//get the input from the interface???
+router.get('/getInput', function(req, res){
     res.send({type: 'GET'});
+});
+
+//OR update the input??
+router.post('/postInput', function(req, res){
+    res.send({type: 'POST'});
+});
+
+//get list of words from the data
+router.get('/data', function(req, res){
+    res.rend({type: 'GET'});
+
+});
+
+
+//update a word to the data
+router.post('/data/:dic', function(req, res){
+    res.send({type: 'POST'});
+});
+
+//DELETE word from database
+router.delete('/data/:dic', function(req, res){
+    res.send({type: 'DELETE'});
 });
